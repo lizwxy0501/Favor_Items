46103d5c5d04f375a9059f722dc47e8df39fb0ee
Wanqi <121428454+Wanqi-Strong@users.noreply.github.com>
2023-02-09 20:52:00 +0000

    debug & a small gift

--- src/main/java/com/example/dao/UserRepository.java
+++ src/main/java/com/example/dao/UserRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query(value = "select * from user where name like '$%?1%'", nativeQuery = true)
-    Page<User> findByNameLike(String name, Pageable pageRequest);
+    public final static String searchSql1 = "SELECT * FROM user WHERE name like %:name%";
+
+    @Query(value = searchSql1, nativeQuery = true)
+    Page<User> findByNameLike(@Param("name")String name, Pageable pageRequest);
 }
